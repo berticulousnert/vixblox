526d626ddea31d1a2ec69d6a3eeffb34fc892c5e
berticulousnert <91563714+berticulousnert@users.noreply.github.com>
2023-11-19 14:35:11 -0800

    tried fixing but reverted

--- functions/v6/getPlayerHeadshot.js
+++ functions/v6/getPlayerHeadshot.js
@@ -13,8 +13,8 @@ module.exports = {
            return vixError(d, "Only one argument is requied and that is: Userid to find the user Optionals: Size: The size of the image to be returned; defaults highest resolution, Format: The file format of the returned thumbnails, IsCircular: Return the circular version of the thumbnails, Croptype: The style of thumbnail that will be returned");
         } else if(user) {
             let userdata = await noblox.getPlayerThumbnail(user, size, format, iscircular, croptype)
-            var commanddata = JSON.stringify(userdata);
-        }
+            var commanddata = JSON.stringify(userdata)
+    }
         data.result = commanddata
     } catch (err) {
         
